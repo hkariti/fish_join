5b0f13ce250c706c54adf27176043b57273225ab
Hagai Kariti <hagaik@campus.technion.ac.il>
2023-08-16 15:50:04 +0300

    Fully automate RS-FISH

--- fiji_module.py
+++ fiji_module.py
@@ -86,31 +86,31 @@ class QuPathSegmentor:
         
 
 class RSFISHSegmentor:
-    def __init__(self, channels):
+    def __init__(self, channels, result_file_pattern="{image_dir}/{image_title}_C{channel}.csv"):
         self.channels = channels
+        self.result_file_pattern = result_file_pattern
 
     def run(self, file_list_path, params_override={}):
-        #results_path_pattern
         default_params = {
     #       "image": imName 
             "mode": "Advanced",
-            "anisotropy": 1.4,
+            "anisotropy": 1.0,
             "robust_fitting": "[RANSAC]",
+            "compute_min/max": True,
             "use_anisotropy": True,
-            "image_min": 190,
-            "image_max": 255,
-            "sigma": 1.4,
-            "threshold": 0.007,
-            "support": 2,
-            "min_inlier_ratio": 0.0,
-            "max_error": 0.9,
-            "spot_intensity_threshold": 0,
+            "spot_intensity": "[Linear Interpolation]",
+            "sigma": 1.4310,
+            "threshold": 0.00219,
+            "support": 3,
+            "min_inlier_ratio": 0.10,
+            "max_error": 1.5,
+            "spot_intensity_threshold": 3094.59,
             "background": "[No background subtraction]",
             "background_subtraction_max_error": 0.05,
-            "background_subtraction_min_inlier_ratio": 0.0,
+            "background_subtraction_min_inlier_ratio": 0.10,
     #       "results_file": "[" + results_csv_path + "]",
             "use_multithreading": True,
-            "num_threads": 40,
+            "num_threads": 10,
             "block_size_x": 128,
             "block_size_y": 128,
             "block_size_z": 16,
@@ -124,24 +124,51 @@ class RSFISHSegmentor:
         
         for file_path in open(file_list_path):
             file_path = file_path.strip()
+            image_dir = os.path.dirname(file_path)
             IJ.log("RSFISHSegmentor: opening image: " + file_path)
             imp = IJ.openImage(file_path)
-            IJ.log("RSFISHSegmentor: splitting to channels")
+            image_title = imp.getShortTitle()
+            IJ.log("RSFISHSegmentor: {}: splitting to channels".format(image_title))
             imp_channels = ChannelSplitter.split(imp)
-            IJ.log("RSFISHSegmentor: image has {} channels, will use: {}".format(len(imp_channels), self.channels))
+            IJ.log("RSFISHSegmentor: {}: image has {} channels, will use {}".format(image_title, len(imp_channels), self.channels))
 
             for ch in self.channels:
-                IJ.log("RSFISHSegmentor: processing channel {}".format(ch))
+                IJ.log("RSFISHSegmentor: {}: processing channel {}".format(image_title, ch))
                 imp_ch = imp_channels[ch-1]
                 params_ch = params[ch]
-                self.process_channel(imp_ch, params_ch)
-            IJ.log("RSFISHSegmentor: done")
+                result_file_path = self.result_file_pattern.format(image_dir=image_dir, image_title=image_title, channel=ch)
+                IJ.log("RSFISHSegmentor: {}: channel {}: saving to {}".format(image_title, ch, result_file_path))
+                self.process_channel(imp_ch, result_file_path, params_ch)
+            IJ.log("RSFISHSegmentor: {}: done".format(image_title))
+        IJ.log("RSFISHSegmentor: finished processing all images")
             
-    def process_channel(self, imp, params):
-        pass
+    def process_channel(self, imp, results_file, params):
+        params['results_file'] = [results_file]
+        #params['image'] = "08_A08.nd2_MAX.tif"
+        params_str = self._create_param_string(params)
+
+        imp.show()
+        IJ.run(imp, "RS-FISH", params_str)
+        imp.hide()
+
+    def _create_param_string(self, params):
+        param_str = ''
+        for key, value in params.items():
+            if type(value) is bool and value:
+                param_str += '{} '.format(key)
+            elif type(value) in [list, tuple]:
+                assert len(value) == 1 # The list is to mark that this is a choice from a drop-down, not to hold several values
+                param_str += '{}=[{}] '.format(key, value[0])
+            elif type(value) is float:
+                param_str += '{}={:.4f} '.format(key, value)
+            else:
+                param_str += '{}={} '.format(key, value)
+
+        return param_str.strip()
+                
 
 file_list = create_file_list(directory, pattern)
 nuclei_segmentor = QuPathSegmentor(nuclei_channel, qupath_executable, tmp_dir, script_dir)
-nuclei_segmentor.run(file_list, params_override=nuclei_params_override)
+#nuclei_segmentor.run(file_list, params_override=nuclei_params_override)
 dots_segmentor = RSFISHSegmentor(channels=dots_channels)
 dots_segmentor.run(file_list, params_override=dots_params_override)
